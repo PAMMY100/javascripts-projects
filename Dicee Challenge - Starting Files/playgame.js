0dab97063050ae0f4bcb46fb040697ab33ac5725
PAMMY100 <princesamuel636@gmail.com>
2024-01-11 21:30:50 +0100

    corrected game players

--- Dicee Challenge - Starting Files/playgame.js
+++ Dicee Challenge - Starting Files/playgame.js
@@ -6,28 +6,28 @@ let img2 = document.querySelector(".img2");
 
 function playGame() {
   //set variable pick for playerOne and Two
-  let playOne = Math.floor(Math.random() * 6) + 1;
-  let playTwo = Math.floor(Math.random() * 6) + 1;
+  let player = Math.floor(Math.random() * 6) + 1;
+  let computer = Math.floor(Math.random() * 6) + 1;
   let result = " ";
 
   //set condition to check who won the roll.
-  if (playOne > playTwo) {
-    result = "Player1 wins.";
-  } else if (playOne < playTwo) {
-    result = "Player2 wins."
-  } else if (playOne === playTwo) {
+  if (player > computer) {
+    result = "Player wins.";
+  } else if (player < computer) {
+    result = "Computer wins."
+  } else if (player === computer) {
     result = "draw";
   }
 
   //set attributes to image to display the dice number
-  img1.setAttribute("src", `images/dice${playOne}.png`);
-  img2.setAttribute("src", `images/dice${playTwo}.png`);
+  img1.setAttribute("src", `images/dice${player}.png`);
+  img2.setAttribute("src", `images/dice${computer}.png`);
 
   //set condition to display winner.
 
-  if (result === 'Player1 wins.') {
+  if (result === 'Player wins.') {
     display.innerHTML = result;
-  } else if (result === 'Player2 wins.') {
+  } else if (result === 'computer wins.') {
     display.innerHTML = result;
   } else if (result === 'draw') {
     display.innerHTML = result;
